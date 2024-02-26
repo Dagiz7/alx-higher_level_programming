aca878cd62bf21d49644f5ccc9892f18cc2ab9d5
Natanim Abesha <natanim@nordstone.co.uk>
2024-02-26 08:48:21 +0300

    my project

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-     sum = 0
+    result = 0
     for arg in sys.argv:
-           if arg != sys.argv[0]:
-        sum += int(arg)
-    print(sum)
+        if arg != sys.argv[0]:
+            result += int(arg)
+    print(result)
